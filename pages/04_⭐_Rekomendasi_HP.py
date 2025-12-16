d3129227edf009efcf43f55e622bab40b5c80dcd
indiraamn <indira.mai@ti.ukdw.ac.id>
2025-12-17 02:38:32 +0700

    update rekomendasi hp
    Replaced os.environ with st.secrets for API key retrieval.

--- pages/04_⭐_Rekomendasi_HP.py
+++ pages/04_⭐_Rekomendasi_HP.py
@@ -1,14 +1,11 @@
 import streamlit as st
 import google.generativeai as genai
-from dotenv import load_dotenv
-import os
 
 # ================= CONFIG API =================
 try:
-    load_dotenv()
-    gemini_key = os.environ["GEMINI_API_KEY"]
-
-    import google.generativeai as genai
+    # GANTI: os.environ -> st.secrets
+    gemini_key = st.secrets["GEMINI_API_KEY"]
+    
     genai.configure(api_key=gemini_key)
     model = genai.GenerativeModel("gemini-2.5-flash")
 except Exception:
